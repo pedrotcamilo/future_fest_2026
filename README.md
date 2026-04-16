1243c9070d2570f802bcd62fc77fa5babe44a9c8
Pedro <rm16387@fiap.com.br>
2026-04-16 13:28:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# future_fest_2026
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/future_fest_2026/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.future_fest_2026
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
