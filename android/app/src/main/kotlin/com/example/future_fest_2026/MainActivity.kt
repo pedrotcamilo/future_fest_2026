f68220d98ad3c4d0d9aa1405777a85e42dcea004
ptc0 <mail@ptc0.cloudns.nz>
2026-05-08 08:03:35 -0300

    Pachu vai refazer td

--- android/app/src/main/kotlin/com/example/future_fest_2026/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.future_fest_2026
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/cores.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-var fgColorGlobal = Color.fromRGBO(2, 52, 143, 255);
